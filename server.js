47042903bf4d4c8af9c8b753e2b864f924aa7d99
chadhindsight <chadrickJ8@gmail.com>
2025-11-13 16:09:20 -0500

    Ad missing path import

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 
 const { getBotResponse } = require("./botLogic");
 const products = require("./products");
